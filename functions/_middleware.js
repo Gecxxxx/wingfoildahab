ea60b2e4485c5d665ef763a83b1cad9dfc834e48
Gecxxxx <152751865+Gecxxxx@users.noreply.github.com>
2026-08-24 11:43:59 +0300

    Add DNS-only www canonical redirect

---
+++ functions/_middleware.js
@@ -0,0 +1,13 @@
+const CANONICAL_HOST = "wingfoildahab.com";
+const WWW_HOST = `www.${CANONICAL_HOST}`;
+
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  if (url.hostname === WWW_HOST) {
+    url.hostname = CANONICAL_HOST;
+    return Response.redirect(url.toString(), 301);
+  }
+
+  return context.next();
+}
